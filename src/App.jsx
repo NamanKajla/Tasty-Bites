da29ef035aad800e3dd5dbd27e7320e226a62041
Naman Kajla <nkajla0906@gmail.com>
2025-04-20 18:28:03 +0530

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Recipes from "./pages/Recipes";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Navbar from './components/Navbar';
 
-const App = () => {
+function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/recipes" element={<Recipes />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-
-    </Routes>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
-};
+}
 
 export default App;
