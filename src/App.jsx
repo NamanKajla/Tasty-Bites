2bdb2cd387fea809fb56423a537dcf1db799e48a
Naman Kajla <nkajla0906@gmail.com>
2025-04-20 18:44:16 +0530

    update App and main

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -6,14 +6,14 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { SearchProvider } from './context/SearchContext';
+import './index.css';
 
-import { BrowserRouter } from "react-router-dom";
-import { SearchProvider } from "./context/SearchContext";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <SearchProvider>
     <BrowserRouter>
-      <SearchProvider>
-        <App />
-      </SearchProvider>
+      <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </SearchProvider>
 );
