7c477fe1e690bba38dd99c815747268bed2c3470
Naman Kajla <nkajla0906@gmail.com>
2025-04-20 17:49:13 +0530

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import About from './pages/About'
-import Home from './pages/Home'
-import Recipes from './pages/Recipes'
-import RecipeDetail from './pages/RecipeDetail'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
 
-function App() {
-
+const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/recipes' element={<Recipes/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/recipe/:id' element={<RecipeDetail/>}/>
-      </Routes>
-    </Router>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/recipes" element={<Recipes />} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
