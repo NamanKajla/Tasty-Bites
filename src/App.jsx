eb0c54b75046a137b56289dffd58f3e25c305de3
Naman Kajla <nkajla0906@gmail.com>
2025-04-20 18:03:28 +0530

    added About and Contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import About from './pages/About'
+import Contact from './pages/Contact'
 import Recipes from "./pages/Recipes";
 
 const App = () => {
@@ -8,6 +10,9 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/recipes" element={<Recipes />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+
     </Routes>
   );
 };
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-
 function About() {
   return (
-    <div>About</div>
-  )
+    <div className="about-page">
+      <h2>About Recipe Finder 🍲</h2>
+      <p>
+        This React app allows users to search for delicious recipes using TheMealDB API.
+        It’s built with Vite, React Router, Context API, and custom CSS. Designed to be responsive and user-friendly!
+      </p>
+    </div>
+  );
 }
 
-export default About
+export default About;
